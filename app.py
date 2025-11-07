b761a412af5b8c10ba71b0776e99599c65297b19
Phatslax <david@davidj.co.uk>
2025-11-07 14:50:09 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # app.py - Van Value Assessment Tool
 import streamlit as st
 import matplotlib.pyplot as plt
+import datetime
 
 # -----------------------
 # Helper Functions
@@ -8,7 +9,6 @@ import matplotlib.pyplot as plt
 
 def calculate_historic_curve(reg_year, purchase_price, current_year, mileage_multiplier):
     years = list(range(reg_year, current_year + 1))
-    # Typical depreciation percentages per year for panel vans
     dep_percent = [0.25, 0.15, 0.10, 0.08, 0.07, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05]
     curve = []
     value = purchase_price
@@ -17,8 +17,4 @@ def calculate_historic_curve(reg_year, purchase_price, current_year, mileage_mul
             value = value * (1 - dep_percent[i])
         else:
             value = value * (1 - dep_percent[-1])
-        curve.append(value * mileage_multiplier)
-    return years, curve
-
-def calculate_future_curve(current_value, start_year, end_year, annual_mileage, default_mileage=10000, dep_rate=0.07):
-    years = list(range(start_year, end_
+        curve.append(value * mileage_multiplie_*
